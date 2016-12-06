66fae2fd316273a586ed4d40b7472f6e4ab853cc
pecetta91 <info@nonnamaria.net>
2016-12-06 22:31:12 +0100

    Add files via upload

--- js/kitchen-sink.js
+++ js/kitchen-sink.js
@@ -13,14 +13,20 @@ var mainView = myApp.addView('.view-main', {});
 // Add another view, which is in right panel
 
 
+var baseurl='http://127.0.0.1/milliont/'
+var IDutente=0;
+
+
+
+
 /* ===== Login screen page events ===== */
 myApp.onPageInit('login-screen-embedded2', function (page) {
 	
 	 $$(page.container).find('.sendform').on('click', function () {
-		
         var email = $$(page.container).find('input[name="email"]').val();
         var password = $$(page.container).find('input[name="pass"]').val();
-		var url = 'http://www.scidoo.com/config/login.php';
+		var url = baseurl+'config/login.php';
+		myApp.showIndicator();
 		$$.ajax({
                 url: url,
                 method: 'POST',
@@ -36,14 +42,26 @@ myApp.onPageInit('login-screen-embedded2', function (page) {
                 success: function (data) {
                     // Find matched items
                  	 //alert(data);
-					var ret=data[0].html;alert(ret);
+					
+					myApp.hideIndicator();
+					var ret=atob(data[0].html);
 					var num=ret.indexOf("error");
 					if(num==-1){
-						var query = {IDstr:"14", IDcli:"18"};
-						//navigation(0,'POST',query);
-						location.href="http://www.scidoo.com";
+						var res = ret.split("_");
+						window.localStorage.setItem("IDcliente", res['0']);
+						if(res['1']!=0){
+							window.localStorage.setItem("IDstruttura", res['1']);
+							var query = {IDstr:res['1'], IDcli:res['0']};
+							navigation(0,'POST',query);
+						}else{
+							myApp.addNotification({
+								message: 'Non puoi accedere. Non sei un titolare di una struttura'
+							});
+						}
 					}else{
-						alert('I Dati immessi non sono corretti. Prego riprovare!');	
+						myApp.addNotification({
+							message: 'I Dati immessi non sono corretti. Prego riprovare!'
+						});
 						mainView.router.back();
 					}
 					 
@@ -53,44 +71,10 @@ myApp.onPageInit('login-screen-embedded2', function (page) {
 
 });
 
+
+
 myApp.onPageInit('login-screen-embedded', function (page) {	
 
-
-/*	 var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August' , 'September' , 'October', 'November', 'December'];
-    var calendarInline = myApp.calendar({
-        container: '#ks-calendar-inline-container',
-        value: [new Date()],
-        weekHeader: false,
-        header: false,
-        footer: false,
-		input: '#kscal',
-
-        toolbarTemplate:
-            '<div class="toolbar calendar-custom-toolbar">' +
-                '<div class="toolbar-inner">' +
-                    '<div class="left">' +
-                        '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
-                    '</div>' +
-                    '<div class="center"></div>' +
-                    '<div class="right">' +
-                        '<a href="#" class="link icon-only"><i class="icon icon-forward"></i></a>' +
-                    '</div>' +
-                '</div>' +
-            '</div>',
-        onOpen: function (p) {
-            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
-            $$('.calendar-custom-toolbar .left .link').on('click', function () {
-                calendarInline.prevMonth();
-            });
-            $$('.calendar-custom-toolbar .right .link').on('click', function () {
-                calendarInline.nextMonth();
-            });
-        },
-        onMonthYearChangeStart: function (p) {
-            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
-        }
-    });
-	*/
 	var calendarDefault = myApp.calendar({
         input: '#kscal',
 		dateFormat: 'dd/mm/yyyy'
@@ -100,7 +84,9 @@ myApp.onPageInit('login-screen-embedded', function (page) {
 		
         var email = $$(page.container).find('input[name="email"]').val();
         var data = $$(page.container).find('input[id="kscal"]').val();
-		var url = 'http://www.scidoo.com/config/logincli.php';
+		var url = baseurl+'config/logincli.php';
+		myApp.showIndicator();
+	
 		
 		$$.ajax({
                 url: url,
@@ -113,6 +99,7 @@ myApp.onPageInit('login-screen-embedded', function (page) {
 					json:1
                 },
                 success: function (data) {
+					myApp.hideIndicator();
                     // Find matched items
 					var ret=data[0].html; 
 					var ret2 = atob(ret);
@@ -122,8 +109,10 @@ myApp.onPageInit('login-screen-embedded', function (page) {
 						navigation(1,'POST',query);
 
 					}else{
-						alert('I Dati immessi non sono corretti. Prego riprovare!');	
-						mainView.router.back();
+						myApp.addNotification({
+							message: 'I Dati immessi non sono corretti. Prego riprovare!'
+						});
+						//mainView.router.back();
 					}
 					 
                 }
@@ -135,78 +124,78 @@ myApp.onPageInit('login-screen-embedded', function (page) {
 
 function navigation(id,met,query){
 
-	var baseurl="http://www.scidoo.com/mobile/";
+	var url=baseurl+"mobile/";
 	var apriurl=new Array('config/profilo.php','config/profilocli.php');
-	var url=baseurl+apriurl[id];
-	
+	var url=url+apriurl[id];
+	myApp.showIndicator();
 	$$.ajax({
             url: url,
                 method: met,
 				dataType: 'json',
                 data: query,
                 success: function (data) {
-                    // Find matched items
+    				myApp.hideIndicator();
 					var ret=data[0].html;
 					var ret2 = atob(ret);
-					mainView.router.loadContent(ret2);
-                }
+				mainView.router.loadContent(ret2);
+         }
      });
 }
 
 function navigationtxt(id,met,query,campo){
-	var baseurl="http://www.scidoo.com/mobile/";
-	var apriurl=new Array('config/profilo/temp.php');
+	var url=baseurl+"mobile/";
 	
-	var url=baseurl+apriurl[id];
+	var apriurl=new Array('config/profilo/temp.php');
+	var url=url+apriurl[id];
+	myApp.showIndicator();
 	$$.ajax({
             url: url,
                 method: met,
 				dataType: 'json',
                 data: query,
                 success: function (data) {
+					myApp.hideIndicator();
                     // Find matched items
 					var ret=data[0].html;
 					var ret2 = atob(ret);
 					
 					$$('#'+campo).html(ret2);
 
-					//$$('#'+campo).html(ret2);
                 }
      });
 }
 
-
-
-
 onloadf();
 
 function onloadf(){
-	var baseurl="http://www.scidoo.com/mobile/";
 	
-	var url='config/controlloini.php';
-	
-	var url=baseurl+url;
-	
-	$$.ajax({
-                url: url,
-                method: 'POST',
-				dataType: 'json',
-                success: function (data) {
-                    // Find matched items
-					var ret=data[0].html; 
-					var ret2 = atob(ret);
-					var num=ret2.indexOf("error");
-					if(num==-1){
-						var arr=new Array();
-						navigation(ret2,'POST',arr)
+	IDutente=window.localStorage.getItem("IDcliente");
+	if(isNaN(IDutente)){
+		
+		var url=baseurl+"mobile/";
+		var url=url+'config/controlloini.php';
+		
+		$$.ajax({
+					url: url,
+					method: 'POST',
+					dataType: 'json',
+					success: function (data) {
+						// Find matched items
+						var ret=data[0].html; 
+						var ret2 = atob(ret);
+						var num=ret2.indexOf("error");
+						if(num==-1){
+							var arr=new Array();
+							navigation(ret2,'POST',arr)
+						}
+						 
 					}
-					 
-                }
-            });
+				});
+	}
 }
 
 function modprofilo(id,campo,tipo,val2,agg){
-	var baseurl="http://www.scidoo.com/";
+	var url=baseurl;
 	switch(val2) {
 			case 8:
 				var val=$('#'+campo).html();
@@ -222,20 +211,25 @@ function modprofilo(id,campo,tipo,val2,agg){
 			break;
 			
 		}
-		var url='config/gestioneprofilo.php';
-		
-		var url=baseurl+url;
-		
-		var query = {val:val, tipo:tipo,IDv:id,val2:val2,json:'1'};
+		var url=url+'config/gestioneprofilo.php';
+	myApp.showIndicator();
+	
+	var query = {val:val, tipo:tipo,IDv:id,val2:val2,json:'1'};
+	
 	$$.ajax({
             url: url,
                 method: 'POST',
                 data: query,
 				dataType: 'json',
                 success: function (data) {
+					myApp.hideIndicator();
                     switch(agg){
 						case 1:
+							myApp.addNotification({
+								message: 'Temperatura Modificata con Successo'
+							});
 						 	navigationtxt(0,'POST',0,'contenutodiv');
+							
 							//navigation(1,'POST','');
 						break;
 						
